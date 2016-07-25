56a69ae6d33cdafb67d04bc1d433af2c1b126f92
Huỳnh Hữu Nghĩa <huynh_huunghia77@yahoo.com>
2016-07-25 19:16:22 +0700

    Add files via upload

---
+++ demoSVN/demoSVN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoSVN
+//
+//  Created by TNKHANH on 7/25/16.
+//  Copyright © 2016 T3H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demoSVN/demoSVN/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  demoSVN
+//
+//  Created by TNKHANH on 7/25/16.
+//  Copyright © 2016 T3H. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"gi cung dc");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
