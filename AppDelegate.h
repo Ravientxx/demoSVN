f4bc40c9646fd79926b8457b2dfaf3d36556f6d5
Huỳnh Hữu Nghĩa <huynh_huunghia77@yahoo.com>
2016-07-25 19:12:36 +0700

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoSVN
+//
+//  Created by TNKHANH on 7/25/16.
+//  Copyright © 2016 T3H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
